844707084876ef062d640c53208a25def8d04848
pizza <bitcoinnftpizzalab@gmail.com>
2023-03-23 02:26:48 +0800

    feat: adjust menu styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex } from '@chakra-ui/react'
 import { Link } from 'react-router-dom';
+import { useScrollDirection } from '../utils/useScrollDirection';
 
 interface HeaderProps {
   className?: string;
@@ -7,17 +8,21 @@ interface HeaderProps {
 
 export function Header(props: HeaderProps) {
   const { className } = props;
+  const direction = useScrollDirection();
 
-  return <Flex maxW="1300px" mx={["20px", "20px", "auto"]} mt="5" borderRadius="5px" fontSize={['10px', '12px', '16px']} gap={["3", "4", "10"]} justifyContent="center" lineHeight="60px" height="60px" bgColor="#ffc803"  className={className}>
-    <Box  ml={["0", "30px"]}>
-    <Link to="/">
-      HOME
-    </Link>
-    </Box>
+  return <>
+    <Flex transition="0.5s" top={direction === 'up' ? '5' : '-50px'} opacity={direction === 'up' ? '1' : '0'} position="fixed" zIndex="2" maxW="1300px" w="90%" mx={["20px", "20px", "auto"]} borderRadius="5px" fontSize={['10px', '12px', '16px']} gap={["3", "4", "10"]} justifyContent="center" lineHeight="60px" height="60px" bgColor="#ffc803" left="50%" transform="translateX(-50%)">
+      <Box ml={["0", "30px"]}>
+        <Link to="/">
+          HOME
+        </Link>
+      </Box>
 
-    {/* <Link to="gallery">GALLERY</Link> */}
-    <a>HOW TO INSCRIBE</a>
-    <a>DONATE</a>
-    <a href="https://twitter.com/pizzalabs_btc" target="_blank">TWITTER</a>
-  </Flex>;
+      {/* <Link to="gallery">GALLERY</Link> */}
+      <a>HOW TO INSCRIBE</a>
+      <a>DONATE</a>
+      <a href="https://twitter.com/pizzalabs_btc" target="_blank">TWITTER</a>
+    </Flex>
+    <Box h="60px"></Box>
+  </>;
 }
---
+++ src/utils/useScrollDirection.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect, useRef } from 'react';
+
+export function useScrollDirection() {
+    const [scrollDirection, setScrollDirection] = useState('');
+    const previousScrollYRef = useRef(0);
+
+    useEffect(() => {
+        function handleScroll() {
+            const currentScrollY = window.scrollY;
+            const previousScrollY = previousScrollYRef.current || currentScrollY;
+
+            if (currentScrollY > previousScrollY) {
+                setScrollDirection('down');
+            } else if (currentScrollY < previousScrollY) {
+                setScrollDirection('up');
+            }
+
+            previousScrollYRef.current = currentScrollY;
+        }
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return scrollDirection;
+}
